9091093ae338667ca0d2894fd061469c8baa249f
kaminfo102 <mirzae.uast@gmail.com>
2025-02-20 16:10:13 -0800

    fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,10 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl" suppressHydrationWarning>
       <head>
-        {/* <link
+        <link
           href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
-        /> */}
+        />
       </head>
       <body className="min-h-screen bg-background font-vazir antialiased">
         <Providers>
